8e639f0ee4a8ec677ea1f40ee49062a716f08852
maratlev <leon24082006@gmail.com>
2025-08-07 01:26:31 +0300

    init_commit

--- src/main.zig
+++ src/main.zig
@@ -13,9 +13,7 @@ pub fn main() !void {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     std.debug.print("Logs from your program will appear here!\n", .{});
 
-    // Uncomment this block to pass the first stage
-    //
-    // var client_addr: posix.sockaddr = undefined;
-    // var client_addr_len: posix.socklen_t = @sizeOf(posix.sockaddr);
-    // _ = try posix.accept(sock_fd, &client_addr, &client_addr_len, posix.SOCK.CLOEXEC);
+    var client_addr: posix.sockaddr = undefined;
+    var client_addr_len: posix.socklen_t = @sizeOf(posix.sockaddr);
+    _ = try posix.accept(sock_fd, &client_addr, &client_addr_len, posix.SOCK.CLOEXEC);
 }
